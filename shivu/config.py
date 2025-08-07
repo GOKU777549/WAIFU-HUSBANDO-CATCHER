d10b9d978757e544ee3e8268b1e7d0943830bf2b
GOKU777549 <b7774455@gmail.com>
2025-08-07 10:13:44 +0530

    Update config.py

---
+++ shivu/config.py
@@ -0,0 +1,27 @@
+class Config(object):
+    LOGGER = True
+
+    # Get this value from my.telegram.org/apps
+    OWNER_ID = "7576729648"
+    sudo_users = ["7576729648", "7692444709"]
+    GROUP_ID = -1002691911300
+    TOKEN = "8106403386:AAGSwhBvzjj1NFFFf13dvtJIEoBRnCw8oCY"
+    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = [
+        "https://telegra.ph/file/b925c3985f0f325e62e17.jpg",
+        "https://telegra.ph/file/4211fb191383d895dab9d.jpg"
+    ]
+
+    SUPPORT_CHAT = "Collect_em_support"
+    UPDATE_CHAT = "Collect_em_support"
+    BOT_USERNAME = "Collect_Em_AllBot"
+    CHARA_CHANNEL_ID = "-1002133191051"
+
+    api_id = 26626068
+    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+
+class Production(Config):
+    LOGGER = True
+
+class Development(Config):
+    LOGGER = True
